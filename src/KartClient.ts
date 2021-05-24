eeac5aac66706d66e3012d9548f7f46c0dabd9d2
MoartMedia <alex789189@gmail.com>
2021-05-25 03:00:06 +0900

    refactor

--- src/KartClient.ts
+++ src/KartClient.ts
@@ -1,11 +1,15 @@
 import got, { Got } from 'got'
 import UserMatchList from './UserMatchList';
+import { StatusCodeErrorHandler } from './NexonError';
+import Match from './Match';
+import type { rawUserMatchList } from '../typings/raw'
 
 export interface UserBasicInfo{
     accessId: string;
     name: string;
     level: number;
 }
+
 export class KartClient {
     private _token: string;
     public api: Got;
@@ -17,27 +21,38 @@ export class KartClient {
             headers: {
                 'Authorization': this._token
             },
-            responseType: 'json'
-        });
+            responseType: 'json',
+            handlers:[StatusCodeErrorHandler]
+        })
         }
-    async getIDByName(name:string){
+    
+    async getUserBasicInfoByName(name:string) : Promise<UserBasicInfo | null>{
         const _name = encodeURIComponent(name);
         const res = await this.api.get(`users/nickname/${_name}`)
+        if(!res) return null;
         return res.body as unknown as UserBasicInfo;
     }
-    async getNameByID(id:string){
-        const res = await this.api.get(`users/${id}`)
+
+    async getUserBasicInfoByID(id:string) : Promise<UserBasicInfo | null>{
+        const res = await this.api.get(`users/${id}`);
+        if(!res) return null;
         return res.body as unknown as UserBasicInfo;
     }
-    async getUserMatchList(info:UserBasicInfo,limit:number=20,offset:number=0){
-        const res = await this.api.get(`users/${info.accessId}/matches`)
-        return new UserMatchList(info,res.body,{limit,offset});
+
+    async getUserMatchList(info:UserBasicInfo,limit:number=20,offset:number=0) : Promise<UserMatchList | null>{
+        const res = await this.api.get(`users/${info.accessId}/matches`);
+        if(!res) return null;
+        const data = res.body as unknown as rawUserMatchList;
+        return new UserMatchList(info,data,{limit,offset});
     }
-    async getMatchInfo(MatchID:string){
+
+    async getMatch(MatchID:string) : Promise<Match | null>{
         const res = await this.api.get(`matches/${MatchID}`);
-
+        if(!res) return null;
+        return new Match(res.body);
     }
-    async getMatchList(limit){
 
-    }
+   /* async getAllMatchList(limit){
+
+    }*/
     }
