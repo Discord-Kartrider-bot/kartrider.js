70c92850b3a91e39f0b710e983af57e4287ec8f8
MoartMedia <alex789189@gmail.com>
2021-05-25 03:02:58 +0900

    chore: thing...

---
+++ src/Player.ts
@@ -0,0 +1,15 @@
+export default class Player{
+public id: string | null;
+public name: string | null;
+public _characterID: string | null;
+public _kartID: string | null;
+public _petID: string | null;
+public _flyingPetID: string | null;
+public matchTime: number | null;
+public matchWin: string | null;
+public _lisenceID: string | null;
+public matchRetired: string | null;
+constructor(data){
+    
+}
+}
