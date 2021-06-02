1c3c35fdb8a6655cedfe500054d2354b1c85fbbd
MoartMedia <alex789189@gmail.com>
2021-06-02 20:06:39 +0900

    feat: hook KartMetaData

--- src/kart/MatchDetail.ts
+++ src/kart/MatchDetail.ts
@@ -1,11 +1,12 @@
+import type { KartMetaData } from '../';
 import type { rawMatchDetail } from '../../typings/raw'
 import Match from './Match';
 export default class MatchDetail extends Match{
     private _playTime: number;
     public gameSpeed: number;
 
-    constructor(data: rawMatchDetail){
-    super(data);
+    constructor(data: rawMatchDetail, kartMetaData?:KartMetaData){
+    super(data,kartMetaData);
     this._playTime = data.playTime;
     this.gameSpeed = data.gameSpeed;
     }
